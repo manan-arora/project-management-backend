e38c5ff1d9333b8ddcc89c0346e7a0a5a6e47539
Manan Arora <mananarora1350@gmail.com>
2026-05-16 22:12:37 +0530

    add .env file to codebase

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config({path:"./.env"});
+
+let myusername = process.env.myusername;
+
+console.log(`username is ${myusername}`);
+
 console.log("Start of backend project");
