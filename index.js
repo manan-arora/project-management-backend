45603907e4135352e0d772382eec6b8c2a2a20d9
Manan Arora <mananarora1350@gmail.com>
2026-05-17 23:30:24 +0530

    build expresss server

--- index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config({path:"./.env"});
-
-let myusername = process.env.myusername;
-
-console.log(`username is ${myusername}`);
-
-console.log("Start of backend project");
