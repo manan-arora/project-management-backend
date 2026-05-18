c44f5dc779298c9ae483585a4eee1ed6e51b16db
Manan Arora <mananarora1350@gmail.com>
2026-05-18 17:21:15 +0530

    connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+//basic express configurations
+app.use(express.json({ limit : "16kb"})); //Parses incoming JSON data from requests.
+app.use(express.urlencoded({extended: true, limit: "16kb"})); //Parses form data sent from HTML forms.
+app.use(express.static("public")); //Serves static files like images, CSS, and JavaScript from the public folder.
+
+//cors configs
+app.use(cors({
+  origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
-import app from "./app.js"
+import app from "./app.js";
+import connectDB from "./db/index.js";
 
-dotenv.config({path:"./.env"});
+dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
+connectDB()
+  .then(
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    }),
+  )
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  });
