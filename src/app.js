b1db8a2ea3614bd3f8fdf6bfface3ec9eb490b3f
Manan Arora <mananarora1350@gmail.com>
2026-05-19 00:44:30 +0530

    add healthcheck controller and router

--- src/app.js
+++ src/app.js
@@ -4,20 +4,28 @@ import cors from "cors";
 const app = express();
 
 //basic express configurations
-app.use(express.json({ limit : "16kb"})); //Parses incoming JSON data from requests.
-app.use(express.urlencoded({extended: true, limit: "16kb"})); //Parses form data sent from HTML forms.
+app.use(express.json({ limit: "16kb" })); //Parses incoming JSON data from requests.
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //Parses form data sent from HTML forms.
 app.use(express.static("public")); //Serves static files like images, CSS, and JavaScript from the public folder.
 
 //cors configs
-app.use(cors({
-  origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//import the routes
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT || 3000;
 
-
 connectDB()
   .then(
     app.listen(port, () => {
@@ -18,3 +17,4 @@ connectDB()
     console.error("MongoDB connection error", err);
     process.exit(1);
   });
+
